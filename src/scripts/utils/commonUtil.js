6844a440b93c130a625f9503b16e363b96864eb3
@guojunmei <2583750585@qq.com>
2016-11-29 17:04:50 +0800

    修改了公共的

--- src/scripts/utils/commonUtil.js
+++ src/scripts/utils/commonUtil.js
@@ -1,7 +1,11 @@
 var commonUtil = {
 	setFocus:function($el){
        $el.addClass("active").siblings().removeClass("active");
+	},
+	set:function(){
+		console.log("aa")
 	}
 }
+
 
 module.exports = commonUtil;
